a801414802042c0c7e2eba858decdb22955293aa
Romain Gallet <romain.gallet@gmail.com>
2025-04-15 22:45:14 +0200

    CVE-3

--- src/main/java/lu/formas/views/dashboard/AddVaccineModal.java
+++ src/main/java/lu/formas/views/dashboard/AddVaccineModal.java
@@ -6,28 +6,40 @@ import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.select.Select;
+import com.vaadin.flow.data.binder.BeanValidationBinder;
+import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.data.renderer.ComponentRenderer;
 import lombok.Getter;
 import lombok.val;
 import lu.formas.services.PatientService;
 import lu.formas.services.VaccineService;
 import lu.formas.services.model.VaccinesByMaturityValue;
+import lu.formas.views.profile.forms.AddVaccineBean;
 
 @Getter
 public class AddVaccineModal extends Dialog {
-    private Select<VaccinesByMaturityValue> select;
+    private Select<VaccinesByMaturityValue> select = new Select<>();
+    private DatePicker datePicker = new DatePicker("Day of vaccination");
     private final VaccineService vaccineService;
+
+    private Binder<AddVaccineBean> binder = new BeanValidationBinder<>(AddVaccineBean.class);
+    private AddVaccineBean bean = new AddVaccineBean();
 
     public AddVaccineModal(PatientService patientService, VaccineService vaccineService) {
         this.vaccineService = vaccineService;
 
+        binder.forField(select).asRequired().bind(AddVaccineBean::getSelect, AddVaccineBean::setSelect);
+        binder.forField(datePicker)
+                .asRequired()
+                .bind(AddVaccineBean::getDatePicker, AddVaccineBean::setDatePicker);
+
         setCloseOnEsc(true);
         setCloseOnOutsideClick(true);
-        setClassName("confirm-delete");
+        setClassName("vaccine-modal");
 
         setMinHeight("500px");
         setMinWidth("800px");
@@ -37,22 +49,30 @@ public class AddVaccineModal extends Dialog {
         val modalLayout = new VerticalLayout();
         val message = new H2("Which vaccine would you like to add?");
         modalLayout.add(message);
+        modalLayout.setWidthFull();
+        modalLayout.setHeightFull();
 
-        val datePicker = new DatePicker("Day of vaccination");
+        datePicker.setRequired(true);
         modalLayout.add(datePicker);
 
         val buttonLayout = new HorizontalLayout() {{
             val addButton = new Button("Add", event -> {
-                close();
+
+                binder.writeBeanIfValid(bean);
+                System.out.println("isValid " + binder.isValid());
+                System.out.println("bean" + bean);
+
+//                close();
             });
             addButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
 
-            val closeButton = new Button("Close", event -> {
-                close();
-            });
+            val closeButton = new Button("Close", event -> close());
 
             add(addButton, closeButton);
         }};
+
+        buttonLayout.setWidthFull();
+        buttonLayout.setJustifyContentMode(FlexComponent.JustifyContentMode.END);
 
         modalLayout.add(select, buttonLayout);
 
@@ -62,7 +82,6 @@ public class AddVaccineModal extends Dialog {
     public void prepareForm() {
         val vaccinesByMaturity = vaccineService.groupedByMaturity();
 
-        select = new Select<>();
         select.setLabel("Select vaccine");
         select.setItems(vaccinesByMaturity.flatten());
         select.setPlaceholder("Select a vaccine");
@@ -71,6 +90,7 @@ public class AddVaccineModal extends Dialog {
                 item -> !item.isPlaceholder());
 
         select.setWidthFull();
+        select.setHeight("80%");
 
         select.setRenderer(
                 new ComponentRenderer<>(vaccine -> new Div(vaccine.getVaccine().getName()))
---
+++ src/main/java/lu/formas/views/profile/forms/AddVaccineBean.java
@@ -0,0 +1,26 @@
+package lu.formas.views.profile.forms;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lu.formas.services.model.VaccinesByMaturityValue;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class AddVaccineBean {
+
+    @NotEmpty(message = "Password is required")
+    @Size(message = "Maximum size is 128", max = 128)
+    private LocalDate datePicker;
+
+    @NotEmpty(message = "Password is required")
+    @Size(message = "Maximum size is 128", max = 128)
+    private VaccinesByMaturityValue select;
+}
