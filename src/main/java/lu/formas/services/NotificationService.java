a4916178444565f9cf6b80cdc9a2a0c8a277316b
Romain Gallet <romain.gallet@gmail.com>
2025-04-17 01:18:02 +0200

    Tests

--- src/main/java/lu/formas/services/NotificationService.java
+++ src/main/java/lu/formas/services/NotificationService.java
@@ -1,23 +1,48 @@
 package lu.formas.services;
 
+import io.vavr.collection.Stream;
+import lombok.val;
 import lu.formas.repository.model.Patient;
+import lu.formas.repository.model.Vaccine;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
 public class NotificationService {
     private final PatientService patientService;
+    private final VaccineService vaccineService;
 
-    public NotificationService(PatientService patientService) {
+    public NotificationService(PatientService patientService, VaccineService vaccineService) {
         this.patientService = patientService;
+        this.vaccineService = vaccineService;
     }
 
     public long ageInMonths(Patient patient) {
         return ChronoUnit.MONTHS.between(patient.getDob(),
                 LocalDate.now().minusDays(1));
+    }
+
+    public List<Vaccine> nextVaccines(Patient patient) {
+        val months = ageInMonths(patient);
+
+        val vaccines = vaccineService.vaccines();
+
+        val futureVaccines = Stream.ofAll(vaccines).filter(v -> v.getMaturityMonth() >= months);
+        val nextEligibleMaturityMonth = futureVaccines.minBy(Vaccine::getMaturityMonth);
+
+        if (nextEligibleMaturityMonth.isEmpty()) { // no more vaccines coming up
+            return Collections.emptyList();
+        }
+
+        val eligibleMaturityMonthVaccines = futureVaccines.filter(e -> e.getMaturityMonth().intValue() == nextEligibleMaturityMonth.get().getMaturityMonth().intValue());
+
+        return eligibleMaturityMonthVaccines.collect(Collectors.toList());
     }
 }
--- src/test/java/lu/formas/services/NotificationServiceTest.java
+++ src/test/java/lu/formas/services/NotificationServiceTest.java
@@ -3,18 +3,20 @@ package lu.formas.services;
 import lombok.SneakyThrows;
 import lombok.val;
 import lu.formas.Application;
-import lu.formas.repository.PatientRepository;
 import lu.formas.repository.model.Patient;
+import lu.formas.repository.model.Vaccine;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDate;
+import java.util.Arrays;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(
@@ -25,10 +27,13 @@ class NotificationServiceTest {
     @MockitoBean
     PatientService patientService;
 
+    @MockitoBean
+    VaccineService vaccineService;
+
     @Test
     @SneakyThrows
-    public void test_Login_True_When_passwords_Match() {
-        val service = new NotificationService(patientService);
+    public void test_Calculate_Age_In_Months() {
+        val service = new NotificationService(patientService, vaccineService);
 
         val patient = new Patient();
 
@@ -40,5 +45,39 @@ class NotificationServiceTest {
 
         patient.setDob(LocalDate.now().minusDays(1).minusMonths(12));
         assertEquals(12, service.ageInMonths(patient));
+    }
+
+    @Test
+    @SneakyThrows
+    public void test_Get_Next_Vaccine() {
+        val service = new NotificationService(patientService, vaccineService);
+
+        val inThePath = new Vaccine();
+        inThePath.setId(0L);
+        inThePath.setName("Vaccine");
+        inThePath.setMaturityMonth(0);
+
+        val nextOne1 = new Vaccine();
+        nextOne1.setId(1L);
+        nextOne1.setName("Vaccine");
+        nextOne1.setMaturityMonth(1);
+
+        val nextOne2 = new Vaccine();
+        nextOne2.setId(11L);
+        nextOne2.setName("Vaccine");
+        nextOne2.setMaturityMonth(1);
+
+        val TooFarInTime = new Vaccine();
+        TooFarInTime.setId(2L);
+        TooFarInTime.setName("Vaccine");
+        TooFarInTime.setMaturityMonth(5);
+
+        Mockito.when(vaccineService.vaccines()).thenReturn(Arrays.asList(inThePath, nextOne1, TooFarInTime, nextOne2));
+
+        val patient = new Patient();
+        patient.setDob(LocalDate.now().minusDays(1).minusMonths(1));
+
+        val nextVaccines = service.nextVaccines(patient);
+        assertEquals(Arrays.asList(nextOne1, nextOne2), nextVaccines);
     }
 }
